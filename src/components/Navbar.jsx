98d8744490337bd39dd86f8c7c47b1051ffae95e
dvnggak <devanggakertawijaya@gmail.com>
2021-12-16 18:05:28 +0700

    styling wrapper

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,31 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
-    background-color: black;
 ` 
+
+const Wrapper = styled.div`
+    padding: 10px 20px ;
+    display: flex;
+    justify-content: space-between;
+`
+const Left = styled.div`
+
+`
+const Center = styled.div`
+
+`
+const Right = styled.div`
+
+`
 
 export default function Navbar() {
     return (
         <Container>
-            container   
+            <Wrapper>
+                <Left>Left</Left>
+                <Center>Center</Center>
+                <Right>Right</Right>
+            </Wrapper>   
         </Container>
     )
 }

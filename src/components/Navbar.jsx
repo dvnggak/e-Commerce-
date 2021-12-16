86c8fcda96a1532fafdc57f6609b8f38d68a4e6f
dvnggak <devanggakertawijaya@gmail.com>
2021-12-16 17:58:08 +0700

    fixing errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const container = styled.div`
-    height : 60px;
+const Container = styled.div`
+    height: 60px;
     background-color: black;
-` ;
+` 
 
 export default function Navbar() {
     return (
-        <container>
-            container
-        </container>
+        <Container>
+            container   
+        </Container>
     )
 }

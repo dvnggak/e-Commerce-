f1d71d403d40a69c05bafdc7da28f472434f2cf9
dvnggak <devanggakertawijaya@gmail.com>
2021-12-16 17:53:08 +0700

    major modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
     return (
         <div>
-            Homepage
+           <Navbar/>
         </div>
     )
 }

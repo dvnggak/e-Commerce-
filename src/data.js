a0546e84d53aae64a95d5e2aec7d7c02944a8771
dvnggak <devanggakertawijaya@gmail.com>
2021-12-18 11:28:07 +0700

    array object in data.js

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/zSw5dCk/1.png",
+        title: "END YEAR SALE",
+        desc: "GET IT NOW ! AND EARN 45% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/zSw5dCk/1.png",
+        title: "NEW YEAR SALE",
+        desc: "GET IT NOW ! AND EARN 45% OFF FOR NEW ARRIVALS",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/zSw5dCk/1.png",
+        title: "BEST SALE",
+        desc: "GET IT NOW ! AND EARN 45% OFF FOR NEW ARRIVALS",
+        bg: "fbf0f4",
+    },
+]

c2bca69a78f3cfffeb7509fa5efefee74b018124
dvnggak <devanggakertawijaya@gmail.com>
2021-12-18 14:51:26 +0700

    data.js new assets

--- src/data.js
+++ src/data.js
@@ -8,14 +8,14 @@ export const sliderItems = [
     },
     {
         id: 2,
-        img: "https://i.ibb.co/zSw5dCk/1.png",
+        img: "https://i.ibb.co/G7fpHTj/fococlipping-20211218-143818.png",
         title: "NEW YEAR SALE",
         desc: "GET IT NOW ! AND EARN 45% OFF FOR NEW ARRIVALS",
         bg: "fcf1ed",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/zSw5dCk/1.png",
+        img: "https://i.ibb.co/M78LLHn/3.png",
         title: "BEST SALE",
         desc: "GET IT NOW ! AND EARN 45% OFF FOR NEW ARRIVALS",
         bg: "fbf0f4",

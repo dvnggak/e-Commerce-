b85a04b4b3d279617ab56d18e183c775d9f21faf
dvnggak <devanggakertawijaya@gmail.com>
2021-12-18 10:16:07 +0700

    slider arrow btn

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,23 +6,36 @@ import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 const Container = styled.div`
     width: 100%;
     height: 100vh;
+    display: flex;
+    background-color: #FFC600;
+    position: relative;
 `
 
 const Arrow = styled.div`
     width: 50px;
     height: 50px;
-    background-color: #fafafa;
+    background-color: #ffffff;
     border-radius: 50%;
-    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction ==="left" && "10px" };
+    right: ${props => props.direction ==="right" && "10px" };
+    cursor: pointer;
+    margin: auto;
+    opacity: 0.7;
 `
 
 export default function Slider() {
     return (
         <Container>
-            <Arrow>
+            <Arrow direction="left" >
                 <ArrowLeftOutlinedIcon/>
             </Arrow>
-            <Arrow>
+            <Arrow direction="right" >
                 <ArrowRightOutlinedIcon/>
             </Arrow>
         </Container>            

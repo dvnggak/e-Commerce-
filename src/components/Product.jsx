459c964ab2cea878d7f0bc6fd716d8f0d8c851c7
dvnggak <devanggakertawijaya@gmail.com>
2021-12-19 10:59:36 +0700

    hover effect on parent component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,20 @@ import styled from 'styled-components'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { FavoriteBorderOutlined, SearchOutlined } from '@mui/icons-material';
 
+const Info = styled.div`
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 3;
+    background-color: rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
 const Container = styled.div`
     flex: 1;
     margin: 5px;
@@ -13,6 +27,10 @@ const Container = styled.div`
     justify-content: center;
     background-color: #f5fbfd;
     position: relative;
+
+    &:hover ${Info}{
+        opacity: 1;
+    }
 `
 
 const Circle = styled.div`
@@ -25,18 +43,6 @@ const Circle = styled.div`
 const Image = styled.img`
     height: 75%;
     z-index: 2;
-`
-const Info = styled.div`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 3;
-    background-color: rgba(0, 0, 0, 0.2);
-    display: flex;
-    align-items: center;
-    justify-content: center;
 `
 const Icon = styled.div`
     width: 40px;

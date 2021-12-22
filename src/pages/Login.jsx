bbea3869b0a8322e62381a9e11760d8d52793f2a
dvnggak <devanggakertawijaya@gmail.com>
2021-12-22 11:03:50 +0700

    Register page styling

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+`
+
+export default function Login() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}

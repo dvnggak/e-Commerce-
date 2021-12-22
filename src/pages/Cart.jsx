581e6b58fa21173233d64df23d829c81552dc277
dvnggak <devanggakertawijaya@gmail.com>
2021-12-22 18:29:48 +0700

    cart styling

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@mui/icons-material'
 import React from 'react'
 import styled from 'styled-components'
 import Announcement from '../components/Announcement'
@@ -44,9 +45,62 @@ const Bottom = styled.div`
 const Info = styled.div`
     flex: 3;
 `
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`
+const Image = styled.img`
+    width: 200px;
+`
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+const ProductName = styled.span`
+
+`
+const ProductId = styled.span`
+
+`
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color}
+`
+const ProductSize = styled.span`
+
+`
+const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+const ProductAmount = styled.div`
+    font-size: 25px;
+    margin: 5px;
+`
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 300;
+`
+
+
 const Summary = styled.div`
     flex: 1;
-    background-color: red;
 `
 
 export default function Cart() {
@@ -65,8 +119,28 @@ export default function Cart() {
                    <TopButton type='filled' >Check Out</TopButton> 
                 </Top>
                 <Bottom>
-                    <Info>i</Info>
-                    <Summary>a</Summary>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
+                                <Details>
+                                    <ProductName> <b>Product :</b> MENS SHOE THUNDER </ProductName>
+                                    <ProductId> <b>ID :</b> 251001OktFri </ProductId>
+                                    <ProductColor color='black' />
+                                    <ProductSize> <b>Size :</b> 43 </ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    <Add/>
+                                    <ProductAmount>3</ProductAmount>
+                                    <Remove/>
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 50</ProductPrice>
+                            </PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary></Summary>
                 </Bottom> 
             </Wrapper>
             <Footer/>

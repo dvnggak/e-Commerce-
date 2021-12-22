b4aeacef0890023dadb69dc5469fef7153ba5d9a
dvnggak <devanggakertawijaya@gmail.com>
2021-12-22 11:15:49 +0700

    login page styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <Register/> ;
+  return <Login/> ;
 };
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,69 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255,255,255,0.5),
+        rgba(255,255,255,0.5)) ,
+        url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") 
+        center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Wrapper = styled.div`
+    width: 25%;
+    padding: 20px;
+    background-color: white;
+    border-radius: 25px;
+`
+const Title = styled.h1`
+    font-size: 25px;
+    font-weight: 300;
+`
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0;
+    padding: 10px;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    font-weight: 500;
+    cursor: pointer;
+    border-radius: 15px;
+    margin: 10px 0px;
+`
+const Link = styled.a`
+    margin: 5px 0px;
+    font-size: 15px;
+    text-decoration: underline;
+    cursor: pointer;
 `
 
 export default function Login() {
     return (
         <Container>
-            
+            <Wrapper>
+                <Title>Sign In </Title>
+                <Form>
+                    <Input placeholder="Username" />
+                    <Input placeholder="Password" />
+                    <Button>Sign In</Button>
+                    <Link>Forgot Password ?</Link>
+                    <Link>Create a New Account</Link>
+                </Form>
+            </Wrapper>
         </Container>
     )
 }

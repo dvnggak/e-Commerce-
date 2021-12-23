491ab94dcdb2cd1ce2feac2021834f67c76a805d
dvnggak <devanggakertawijaya@gmail.com>
2021-12-23 18:43:52 +0700

    mongose connect and secretkey

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 const app = express();
 
-app.listen(5000, () => {
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_CONNECTION
+).then(()=>{
+    console.log("DB Connection Successful")
+}).catch((error)=> {
+    console.log(error)
+});
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server is Running on Port 5000")
 });

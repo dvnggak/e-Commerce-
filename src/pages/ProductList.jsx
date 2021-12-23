4f465b9351a45d93a305d782adebfc7ed2530ca8
dvnggak <devanggakertawijaya@gmail.com>
2021-12-23 12:48:51 +0700

    productList page responsive

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -5,6 +5,7 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
 
@@ -18,15 +19,18 @@ const FilterContainer = styled.div`
 `
 const Filter = styled.div`
     margin: 20px;
+    ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column"})};
 `
 const FilterText = styled.span`
     font-size: 20px;
     font-weight: 500px;
     margin-right: 20px;
+    ${mobile({ marginRight: "0px", fontSize: "15px"})};
 `
 const Select = styled.select`
     padding: 10px;
     margin-right: 20px;
+    ${mobile({ margin: "10px opx"})};
 `
 const Option = styled.option`
 

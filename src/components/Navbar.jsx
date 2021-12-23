c9d8034875c1451dc4262412e3660fe868624c79
dvnggak <devanggakertawijaya@gmail.com>
2021-12-23 12:12:34 +0700

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,11 @@ import React from 'react';
 import styled from 'styled-components';
 import Badge from '@mui/material/Badge';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import {mobile} from '../responsive'
 
 const Container = styled.div`
     height: 60px;
-    @media only screen and (max-width:380px) {
-        display: none;
-    }
+    ${mobile({ height: "50px"})};
 ` 
 
 const Wrapper = styled.div`
@@ -16,6 +15,7 @@ const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    ${mobile({ padding: "10px 0px;"})};
 `
 const Left = styled.div`
     flex: 1;
@@ -26,6 +26,7 @@ const Left = styled.div`
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
+    ${mobile({ display: "none"})};
 `
 
 const SearchContainer = styled.div`
@@ -34,10 +35,12 @@ const SearchContainer = styled.div`
     align-items: center;
     margin-left: 25px;
     padding: 5px;
+    ${mobile({ marginLeft: "10px", marginRight: "20px"})};
 `
 
 const Input = styled.input`
     border: none;
+    ${mobile({ width: "80px"})};
 `
 
 const Center = styled.div`
@@ -46,6 +49,7 @@ const Center = styled.div`
 `
 const Logo = styled.h1`
     font-weight: 800;
+    ${mobile({ fontSize: "15px"})};
 `
 
 const Right = styled.div`
@@ -53,12 +57,14 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile({ flex: "2", justifyContent: "center"})};
 `
 
 const MenuItem = styled.div`
     font-size: 14;
     cursor: pointer;
     margin-left: 25px;
+    ${mobile({ fontSize: "10px", marginLeft: "10px"})};
 `
 
 export default function Navbar() {
@@ -68,7 +74,7 @@ export default function Navbar() {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        <Input/>
+                        <Input placeholder='Search' />
                         <Search style={{color:"gray", fontSize:16}} />
                     </SearchContainer>
                 </Left>

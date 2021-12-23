38f81836cbe3c73aafc7b67abe4a9e38f6ba6fa2
dvnggak <devanggakertawijaya@gmail.com>
2021-12-23 12:22:52 +0700

    newsletter responsive

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import SendIcon from '@mui/icons-material/Send';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
     height: 60vh;
@@ -9,15 +10,18 @@ const Container = styled.div`
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    ${mobile({ height: "50vh"})};
 `
 const Title = styled.h1`
     font-size: 70px;
     margin-bottom: 20px;
+    ${mobile({ fontSize: "55px"})};
 `
 const Desc = styled.div`
     font-size: 24px;
     font-weight: 300;
     margin-bottom: 20px;
+    ${mobile({ textAlign: "center", fontSize: "18px"})};
 `
 const InputContainer = styled.div`
     width: 50%;
@@ -26,6 +30,7 @@ const InputContainer = styled.div`
     display: flex;
     justify-content: space-between;
     border: 1px solid lightgray;
+    ${mobile({ width: "80%"})};
 `
 const Input = styled.input`
     border: none;

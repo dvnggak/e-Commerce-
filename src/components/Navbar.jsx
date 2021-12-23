2b4377c2fa0fbfdeb6050f77178fbbdbff3d7c49
dvnggak <devanggakertawijaya@gmail.com>
2021-12-23 11:52:41 +0700

    responsive handler js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,9 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Container = styled.div`
     height: 60px;
+    @media only screen and (max-width:380px) {
+        display: none;
+    }
 ` 
 
 const Wrapper = styled.div`

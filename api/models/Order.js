a7acc972501fb54b2cbc373de1a31565fb50a9f8
dvnggak <devanggakertawijaya@gmail.com>
2021-12-24 10:27:51 +0700

    models schema

---
+++ api/models/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');;
+
+const CartOrderSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        products: [
+            {
+                productId : {
+                    type: String,
+                },
+                quantity: {
+                    type: Number,
+                    default: 1,
+                },
+            },
+        ],
+        amount: {
+            type: Number,
+            required: true,
+        },
+        address: {
+            type: Object,
+            required: true,
+        }
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("CartOrder", CartOrderSchema)

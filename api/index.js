997c4f5814a1039557c389d7fed0e79ec1806240
dvnggak <devanggakertawijaya@gmail.com>
2021-12-24 10:56:19 +0700

    new routes and user auth

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 const app = express();
 
+const authRoute = require('./routes/auth')
 const userRoute = require('./routes/user')
 
 dotenv.config();
@@ -16,7 +17,8 @@ mongoose.connect(
 });
 
 app.use(express.json())
-app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is Running on Port 5000")
---
+++ api/routes/auth.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const User = require('../models/User')
+
+// REGISTER USER
+router.post("/register", async (req,res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    try{
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser)
+    }catch (error) {
+        res.status(500).json(error)
+    };
+
+});
+
+module.exports = router;

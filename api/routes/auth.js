75dfc7489b7a05ed5634a5708c88660f79f70f28
dvnggak <devanggakertawijaya@gmail.com>
2021-12-26 18:12:10 +0700

    handling userinfo update

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -8,7 +8,10 @@ router.post("/register", async (req,res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECKEY).toString(),
+        password: CryptoJS.AES.encrypt(
+            req.body.password, 
+            process.env.PASS_SECKEY
+        ).toString(),
     });
 
     try{
---
+++ api/routes/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token
+    if (authHeader) {
+        jwt.verify(token, process.env.JWT_SECKEY, (err, user) => {
+            if (err) res.status(403).json("Token Invalid");
+            req.user = user;
+            next();
+        })
+    } else {
+        return res.status(401).json("Not Authenticated")
+    }
+};
+
+const verifyTokenAndAuthorization = (req,res,next) => {
+    verifyToken(req,res,() => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("Forbidden Act")
+        };
+    });
+};
+
+module.exports = {
+    verifyToken,
+    verifyTokenAndAuthorization,
+};

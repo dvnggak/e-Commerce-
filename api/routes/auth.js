8d5e159a575e021c8cd597a87fe68c417666977a
dvnggak <devanggakertawijaya@gmail.com>
2021-12-26 17:34:01 +0700

    minor changes for spread info in db

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -47,7 +47,7 @@ router.post('/login', async (req, res) => {
 
         const {password, ...others} = user._doc;
 
-        res.status(200).json(others, accessToken);
+        res.status(200).json({...others, accessToken});
     } catch (error) {
         res.status(500).json(error)
     };

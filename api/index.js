d13283bcaf71f77cd51cf08375339755c5337cc5
dvnggak <devanggakertawijaya@gmail.com>
2021-12-27 17:53:24 +0700

    order add and fetch

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const app = express();
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/user')
 const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
 
 dotenv.config();
 
@@ -21,6 +23,8 @@ app.use(express.json())
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is Running on Port 5000")

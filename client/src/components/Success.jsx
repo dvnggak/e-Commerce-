3c71c19b40879f3418988dac02fe2e93a343ecde
dvnggak <devanggakertawijaya@gmail.com>
2022-01-03 11:30:57 +0700

    success components

--- client/src/components/Success.jsx
+++ client/src/components/Success.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 export default function Success() {
+    const location = useLocation();
+     //in Cart.jsx I sent data and cart. Please check that page for the changes.(in video it's only data)
+    const data = location.state.stripeData;
+    const cart = location.state.cart;
+    const currentUser = useSelector((state) => state.user.currentUser);
+    const [orderId, setOrderId] = useState(null);
+
     return (
         <div>
             

228c0cd4511e038796a716334f06a9f7f9ce59f8
dvnggak <devanggakertawijaya@gmail.com>
2022-01-09 11:43:25 +0700

    cart pages useState

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import { Add, Remove } from '@mui/icons-material'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
@@ -141,6 +142,9 @@ const Button = styled.button`
 `
 
 export default function Cart() {
+
+    const cart = useSelector((state) => state.cart)
+
     return (
         <Container>
             <Navbar/>
@@ -157,51 +161,34 @@ export default function Cart() {
                 </Top>
                 <Bottom>
                     <Info>
-                        <Product>
-                            <ProductDetail>
-                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
-                                <Details>
-                                    <ProductName> <b>Product :</b> MENS SHOE THUNDER </ProductName>
-                                    <ProductId> <b>ID :</b> 251001OktFri </ProductId>
-                                    <ProductColor color='black' />
-                                    <ProductSize> <b>Size :</b> 43 </ProductSize>
-                                </Details>
-                            </ProductDetail>
-                            <PriceDetail>
-                                <ProductAmountContainer>
-                                    <Add/>
-                                    <ProductAmount>3</ProductAmount>
-                                    <Remove/>
-                                </ProductAmountContainer>
-                                <ProductPrice>$ 50</ProductPrice>
-                            </PriceDetail>
-                        </Product>
+                        {cart.products.map((product) => (
+                            <Product>
+                                <ProductDetail>
+                                    <Image src={product.img} />
+                                    <Details>
+                                        <ProductName> <b>Product :</b> {product.title} </ProductName>
+                                        <ProductId> <b>ID :</b> {product._id} </ProductId>
+                                        <ProductColor color={product.color} />
+                                        <ProductSize> <b>Size :</b> {product.size} </ProductSize>
+                                    </Details>
+                                </ProductDetail>
+                                <PriceDetail>
+                                    <ProductAmountContainer>
+                                        <Add/>
+                                        <ProductAmount>{product.quantity}</ProductAmount>
+                                        <Remove/>
+                                    </ProductAmountContainer>
+                                    <ProductPrice>$ {product.price * product.quantity}</ProductPrice>
+                                </PriceDetail>
+                            </Product>
+                        ))}
                         <Hr/>
-                        <Product>
-                            <ProductDetail>
-                                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
-                                <Details>
-                                    <ProductName> <b>Product :</b> Philz Tees Men </ProductName>
-                                    <ProductId> <b>ID :</b> 14101976OktMon </ProductId>
-                                    <ProductColor color='gray' />
-                                    <ProductSize> <b>Size :</b> M </ProductSize>
-                                </Details>
-                            </ProductDetail>
-                            <PriceDetail>
-                                <ProductAmountContainer>
-                                    <Add/>
-                                    <ProductAmount>1</ProductAmount>
-                                    <Remove/>
-                                </ProductAmountContainer>
-                                <ProductPrice>$ 17</ProductPrice>
-                            </PriceDetail>
-                        </Product>
                     </Info>
                     <Summary>
                         <SummaryTitle>Order Summary</SummaryTitle>
                         <SummaryItem>
                             <SummaryItemText>Subtotal</SummaryItemText>
-                            <SummaryItemPrice>$ 180</SummaryItemPrice>
+                            <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
                         </SummaryItem>
                         <SummaryItem>
                             <SummaryItemText>Estimated Shipping</SummaryItemText>
@@ -213,7 +200,7 @@ export default function Cart() {
                         </SummaryItem>
                         <SummaryItem type="total">
                             <SummaryItemText>Total</SummaryItemText>
-                            <SummaryItemPrice>$ 180</SummaryItemPrice>
+                            <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
                         </SummaryItem>
                         <Button>Checkout Now</Button>
                     </Summary>

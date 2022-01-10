d936cff62b9d1a5d1f230ffabddcad2cee14b923
dvnggak <devanggakertawijaya@gmail.com>
2022-01-10 14:04:12 +0700

    success pages

---
+++ frontend/src/pages/Success.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+export default function Success() {
+
+    const location = useLocation()
+    console.log(location)
+
+    return (
+        <div>
+            Your Payment is Successfull
+        </div>
+    )
+}

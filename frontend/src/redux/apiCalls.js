329055168a0b054abd1fdcdac7e457aa5ce1f7a0
dvnggak <devanggakertawijaya@gmail.com>
2022-01-10 14:39:59 +0700

    catch data on success

--- frontend/src/redux/apiCalls.js
+++ frontend/src/redux/apiCalls.js
@@ -5,7 +5,7 @@ export const login = async (dispatch, user) => {
     dispatch(loginStart());
     try {
         const res = await publicRequest.post("/auth/login", user);
-        dispatch(loginSuccess())
+        dispatch(loginSuccess(res.data))
     } catch (error) {
         dispatch(loginFailure())
     }

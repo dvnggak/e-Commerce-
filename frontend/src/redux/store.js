1f3a25bea8c4f8d2e0224cee6a3ad5ef99a8843f
dvnggak <devanggakertawijaya@gmail.com>
2022-01-10 14:31:42 +0700

    import userReducer from cartRedux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  cartReducer from "./cartRedux";
+import  userReducer from "./cartRedux";
 
 
 export default configureStore({
     reducer: {
         cart: cartReducer,
-    }
+        user: userReducer,
+    },
 })

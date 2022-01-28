ed60d9352554a844665b2dce205ed1f197b5de30
dvnggak <devanggakertawijaya@gmail.com>
2022-01-28 15:55:41 +0700

    downgrade react-router-dom to v5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,11 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-
 import {
-  BrowserRouter,
-  Routes,
+  BrowserRouter as Router,
+  Switch,
   Route,
-  Navigate,
+  Redirect,
 } from "react-router-dom";
 import Success from "./pages/Success";
 import { useSelector } from "react-redux";
@@ -18,33 +17,29 @@ const App = () => {
   const user = useSelector((state) => state.user.currentUsser);
 
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products/:category" element={<ProductList />} />
-            <Route path="/product/:id" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/success" element={<Success />} />
-            <Route
-              path="/login" 
-              element=
-                {
-                  user 
-                  ? <Navigate to="/" replace="true"/> 
-                  : <Login/> 
-                }
-              />
-            <Route
-              path="/register" 
-              element=
-                {
-                  user 
-                  ? <Navigate to="/" replace="true"/> 
-                  : <Register/> 
-                }
-            />
-        </Routes>
-    </BrowserRouter>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 

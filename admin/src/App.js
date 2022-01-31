a0600839274593a5f2cd5da64a05bfcb09ec4779
dvnggak <devanggakertawijaya@gmail.com>
2022-01-31 18:33:16 +0700

    fix error

--- admin/src/App.js
+++ admin/src/App.js
@@ -13,7 +13,7 @@ import { Login } from "./pages/login/Login";
 
 function App() {
   
-  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin;
 
   return (
     <Router>

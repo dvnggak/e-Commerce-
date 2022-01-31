2b354b2796a4e2e316af752e9c76045f103bcb58
dvnggak <devanggakertawijaya@gmail.com>
2022-01-31 11:03:55 +0700

    new route on App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,6 +9,7 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import { Login } from "./pages/login/Login";
 
 function App() {
   return (
@@ -37,6 +38,9 @@ function App() {
           </Route>
           <Route path="/newproduct">
             <NewProduct />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
       </div>

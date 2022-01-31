dac683e778cb167129d1664e55f33360a6ac02ea
dvnggak <devanggakertawijaya@gmail.com>
2022-01-31 17:56:17 +0700

    fix error

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 const BASE_URL = "http://localhost:5000/api/";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
+console.log(TOKEN);
+
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
 });
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    header: {token:`Bearer ${TOKEN}`}
+    headers: {token:`Bearer ${TOKEN}`}
 });

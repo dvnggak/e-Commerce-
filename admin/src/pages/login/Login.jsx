1ba7a6dde1b4e42051d2f7097efd8abe3597603f
dvnggak <devanggakertawijaya@gmail.com>
2022-01-31 11:16:42 +0700

    add flexDirection

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -19,6 +19,7 @@ export const Login = () => {
             style={{
                 height:"100vh",
                 display:"flex",
+                flexDirection:"column",
                 alignItems:"center",
                 justifyContent:"center",
             }}    

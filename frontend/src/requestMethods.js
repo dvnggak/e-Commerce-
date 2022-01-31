ae316dbee439d7c9ed7b6ad18672f03dc43065ed
dvnggak <devanggakertawijaya@gmail.com>
2022-01-31 17:01:31 +0700

    get TOKEN dynamically

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYzk4NDAyMzljNWYxYmJlZGY2MDRmNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTYxNjM2MCwiZXhwIjoxNjQxODc1NTYwfQ.LLYliKrZz2jHH62SvHk1PQDaD4P7AQEbsnc2rtUDDqA";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,

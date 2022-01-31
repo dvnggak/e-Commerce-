98641e47abbd29f30ecf685ccfa80ecc66bd4d5b
dvnggak <devanggakertawijaya@gmail.com>
2022-01-31 11:32:02 +0700

    parsing the persist:root

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYzk4NDAyMzljNWYxYmJlZGY2MDRmNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTYxNjM2MCwiZXhwIjoxNjQxODc1NTYwfQ.LLYliKrZz2jHH62SvHk1PQDaD4P7AQEbsnc2rtUDDqA";
+const TOKEN = "";
+console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).currentUser).accessToken);
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,

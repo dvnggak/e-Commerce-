72d73bc629d3aa267cbf5d1c37f8c6559ed9e521
dvnggak <devanggakertawijaya@gmail.com>
2022-01-31 09:52:49 +0700

    input forrm, button, inclick & onChange

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Login = () => {
 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleClick = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div>
-
+            <input 
+                type="text" 
+                placeholder='username'
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input 
+                type="password" 
+                placeholder='password'
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleClick} ></button>
         </div>
     );
 };

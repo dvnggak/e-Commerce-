f3140a4dc5c4741de69e3fec5b1bca9f8df444cc
dvnggak <devanggakertawijaya@gmail.com>
2022-02-01 15:46:27 +0700

    Home useState for fetching user data

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -4,8 +4,30 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { useMemo, useState } from "react";
 
 export default function Home() {
+
+  const [ userStats, setUserStats ] = useState([]);
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  )
+
   return (
     <div className="home">
       <FeaturedInfo />

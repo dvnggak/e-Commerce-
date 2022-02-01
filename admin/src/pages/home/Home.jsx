cde3a9f960d7bd396aba4e0d44c3f62764378d3c
dvnggak <devanggakertawijaya@gmail.com>
2022-02-01 16:07:24 +0700

    add key to widgetLg

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -4,7 +4,8 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function Home() {
 
@@ -26,12 +27,28 @@ export default function Home() {
       "Dec",
     ],
     []
-  )
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("/users/stats");
+        res.data.map((item) => 
+          setUserStats((prev) => [
+            ...prev,
+            {name:MONTHS[item._id -1], "Active Users":item.total},
+          ])
+        )
+      } catch {}
+    }
+    getStats();
+  }, [MONTHS]);
+
 
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart data={userStats} title="User Analytics" grid dataKey="Active User"/>
       <div className="homeWidgets">
         <WidgetSm/>
         <WidgetLg/>

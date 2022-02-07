2976fa36655f0c0f8c35b6b09d3307f4c75ccd7c
dvnggak <devanggakertawijaya@gmail.com>
2022-02-07 13:54:02 +0700

    useSelector for produtList

--- admin/src/pages/productList/ProductList.jsx
+++ admin/src/pages/productList/ProductList.jsx
@@ -4,13 +4,15 @@ import { DeleteOutline } from "@material-ui/icons";
 import { productRows } from "../../dummyData";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getProducts } from "../../redux/apiCalls";
 
 export default function ProductList() {
   const [data, setData] = useState(productRows);
 
   const dispatch = useDispatch();
+
+  const products = useSelector((state) => state.product.products);
 
   useEffect(() => {
     getProducts(dispatch)
@@ -69,9 +71,10 @@ export default function ProductList() {
   return (
     <div className="productList">
       <DataGrid
-        rows={data}
+        rows={products}
         disableSelectionOnClick
         columns={columns}
+        getRowId={(row) => row._id}
         pageSize={8}
         checkboxSelection
       />

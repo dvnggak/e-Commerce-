079221e64ff3f125b0c0070b75acba55c5fa67d8
dvnggak <devanggakertawijaya@gmail.com>
2022-02-07 13:45:43 +0700

    fix error

--- admin/src/redux/apiCalls.js
+++ admin/src/redux/apiCalls.js
@@ -14,7 +14,7 @@ export const login = async (dispatch, user) => {
 export const getProducts = async (dispatch) => {
     dispatch(getProductStart());
     try {
-        const res = await publicRequest.post("/products");
+        const res = await publicRequest.get("/products");
         dispatch(getProductSuccess(res.data))
     } catch (error) {
         dispatch(getProductFailure())

5836d2500b30efac91d23826bd7fdc19582855ee
dvnggak <devanggakertawijaya@gmail.com>
2022-02-09 14:12:50 +0700

    fetching product

--- admin/src/pages/product/Product.jsx
+++ admin/src/pages/product/Product.jsx
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./product.css";
 import Chart from "../../components/chart/Chart"
 import {productData} from "../../dummyData"
 import { Publish } from "@material-ui/icons";
+import { useSelector } from "react-redux";
 
 export default function Product() {
+    const location = useLocation()
+    const productId = location.pathname.split("/")[2]
+
+    const product = useSelector((state) => 
+        state.product.products.find((product) => product._id === productId)
+    );
   return (
     <div className="product">
       <div className="productTitleContainer">
@@ -19,25 +26,21 @@ export default function Product() {
           </div>
           <div className="productTopRight">
               <div className="productInfoTop">
-                  <img src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="productInfoImg" />
-                  <span className="productName">Apple Airpods</span>
+                  <img src={product.img} alt="" className="productInfoImg" />
+                  <span className="productName">{product.title}</span>
               </div>
               <div className="productInfoBottom">
                   <div className="productInfoItem">
                       <span className="productInfoKey">id:</span>
-                      <span className="productInfoValue">123</span>
+                      <span className="productInfoValue">{product._id}</span>
                   </div>
                   <div className="productInfoItem">
                       <span className="productInfoKey">sales:</span>
                       <span className="productInfoValue">5123</span>
                   </div>
                   <div className="productInfoItem">
-                      <span className="productInfoKey">active:</span>
-                      <span className="productInfoValue">yes</span>
-                  </div>
-                  <div className="productInfoItem">
                       <span className="productInfoKey">in stock:</span>
-                      <span className="productInfoValue">no</span>
+                      <span className="productInfoValue">{product.inStock}</span>
                   </div>
               </div>
           </div>
@@ -46,16 +49,15 @@ export default function Product() {
           <form className="productForm">
               <div className="productFormLeft">
                   <label>Product Name</label>
-                  <input type="text" placeholder="Apple AirPod" />
+                  <input type="text" placeholder={product.title} />
+                  <label>Product Description</label>
+                  <input type="text" placeholder={product.desc} />
+                  <label>Price</label>
+                  <input type="text" placeholder={product.price} />
                   <label>In Stock</label>
                   <select name="inStock" id="idStock">
-                      <option value="yes">Yes</option>
-                      <option value="no">No</option>
-                  </select>
-                  <label>Active</label>
-                  <select name="active" id="active">
-                      <option value="yes">Yes</option>
-                      <option value="no">No</option>
+                      <option value="true">Yes</option>
+                      <option value="false">No</option>
                   </select>
               </div>
               <div className="productFormRight">
